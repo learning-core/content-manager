453c429c3a7342e193bbd5c8146cdeda3ab52458
Victor Alves <accenture.valves@bv.com.br>
2022-09-10 22:49:06 -0300

    first commit

---
+++ src/test/java/com/wolfleaning/contentmanager/ContentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfleaning.contentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
