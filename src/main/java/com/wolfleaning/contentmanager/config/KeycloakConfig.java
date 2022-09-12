3aa3946ea24960ef36b5c9fce3bcf9f7c6320317
Wolf Develloper <wolfdevelloper@gmail.com>
2022-09-11 23:47:45 -0300

    WL-1 keycloack flow

---
+++ src/main/java/com/wolfleaning/contentmanager/config/KeycloakConfig.java
@@ -0,0 +1,15 @@
+package com.wolfleaning.contentmanager.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//definimos nessa classe que utilizaremos o arquivo application ao inves do padrao do keycloak
+@Configuration
+public class KeycloakConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
