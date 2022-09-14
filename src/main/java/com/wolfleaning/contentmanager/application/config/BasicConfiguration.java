bc12b588fc1b38b512ced8b10b1375dd2cf4370f
Wolf Develloper <wolfdevelloper@gmail.com>
2022-09-14 04:12:25 -0300

    initial config spring security

--- src/main/java/com/wolfleaning/contentmanager/application/config/BasicConfiguration.java
+++ src/main/java/com/wolfleaning/contentmanager/application/config/BasicConfiguration.java
@@ -1,4 +1,41 @@
 package com.wolfleaning.contentmanager.application.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class BasicConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .cors()
+                .and().
+                authorizeRequests().
+                antMatchers(HttpMethod.POST).
+                hasRole("api").
+                anyRequest().
+                authenticated().and()
+                .oauth2ResourceServer().and()
+        .build();
+
+    }
+    private JwtAuthenticationConverter getJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthoritiesClaimName("authorities");
+        converter.setAuthorityPrefix("");
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(converter);
+        return authenticationConverter;
+    }
+
 }
