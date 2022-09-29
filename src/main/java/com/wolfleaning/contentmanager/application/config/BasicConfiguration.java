edb249364d8fa1c7d7941a86f61b48caee4eee86
Wolf Develloper <wolfdevelloper@gmail.com>
2022-09-28 22:05:31 -0300

    WL-1 ENABLE WEB SECUITY WHIT ERROR

--- src/main/java/com/wolfleaning/contentmanager/application/config/BasicConfiguration.java
+++ src/main/java/com/wolfleaning/contentmanager/application/config/BasicConfiguration.java
@@ -3,15 +3,26 @@ package com.wolfleaning.contentmanager.application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;*/
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class BasicConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/cadastrar")
+                .access("hasAuthority('SCOPE_acesso-total')")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
    /*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
